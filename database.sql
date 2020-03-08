2921125888a92160fbbc5e21e7889939b665f63c
felixcloud6288 <mweston6288@knights.ucf.edu>
2020-03-08 13:41:33 -0400

    Created basic SQL database template

---
+++ database.sql
@@ -0,0 +1,37 @@
+DROP DATABASE IF EXISTS employeesDB;
+
+CREATE DATABASE employeesDB;
+
+USE employeesDB;
+
+CREATE TABLE department(
+    id INT AUTO_INCREMENT NOT NULL,
+    name VARCHAR(30) NULL,
+    PRIMARY KEY(id)
+);
+
+CREATE TABLE role(
+    id INT AUTO_INCREMENT NOT NULL,
+    title VARCHAR(30) NULL,
+    salary DECIMAL(10,2) NULL,
+    department_id INT NULL,
+    PRIMARY KEY(id)
+);
+
+CREATE TABLE employee(
+    id INT AUTO_INCREMENT NOT NULL,
+    first_name VARCHAR(30) NULL,
+    last_name VARCHAR(30) NULL,
+    role_id INT NULL,
+    manager_id INT NULL,
+    PRIMARY KEY(id)
+);
+
+INSERT INTO department (name)
+VALUES ("test"), ("test2");
+
+INSERT INTO role (title, salary, department_id)
+VALUES ("Manager", 15.43, 1), ("person", 15.43, 2);
+
+INSERT INTO employee(first_name, last_name, role_id, manager_id)
+VALUES ("Larry", "Smith", 2, 1), ("Larry", "Smith", 2, 1);
