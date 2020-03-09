9a9bc67bb40a62d3d2dda77101040905a8b4a6fc
felixcloud6288 <mweston6288@knights.ucf.edu>
2020-03-09 16:26:27 -0400

    Add role and employee implemented

--- server.js
+++ server.js
@@ -38,7 +38,11 @@ function menu(){
                 addDepartment();
                 break;
             case "Add role":
+                addRole();
+                break;
             case "Add employee":
+                addEmployee();
+                break;
             case "View departments":
             case "View roles":
             case "View employees":
@@ -68,6 +72,92 @@ function addDepartment(){
         }).then(function(response){
             if (response.response === "Yes"){
                 addDepartment();
+            }
+            else{
+                menu();
+            }
+        })
+    })
+}
+// Add a new department to the database
+function addRole(){
+    inquirer.prompt([
+        {
+        name: "title",
+        type: "input",
+        message: "Role title:"
+        },
+        {
+            name: "salary",
+            type: "input",
+            message: "Salary:"
+        },       
+        {
+            name: "department",
+            type: "input",
+            message: "Department id:"
+        }
+        ]).then(function(response){
+        connection.query("INSERT INTO role SET ?",
+        {
+            title: response.title,
+            salary: response.salary,
+            department_id: response.department
+        })
+        inquirer.prompt({
+            name: "response",
+            type: "list",
+            message: "Add another role?",
+            choices: ["Yes", "No"]
+        }).then(function(response){
+            if (response.response === "Yes"){
+                addRole();
+            }
+            else{
+                menu();
+            }
+        })
+    })
+}// Add a new department to the database
+function addEmployee(){
+    inquirer.prompt([
+        {
+            name: "firstName",
+            type: "input",
+            message: "Employee first name:"
+        },
+        {
+            name: "lastName",
+            type: "input",
+            message: "Employee last name:"
+        },
+        {
+            name: "roleID",
+            type: "input",
+            message: "Employee role ID:"
+        },
+        {
+            name: "managerID",
+            type: "input",
+            message: "Manager ID:"
+        },
+
+    ]).then(function(response){
+        connection.query("INSERT INTO employee SET ?",
+        {
+            first_name: response.firstName,
+            last_name: response.lastName,
+            role_id: response.roleID,
+            manager_id: response.managerID
+        })
+        inquirer.prompt({
+            name: "response",
+            type: "list",
+            message: "Add another employee?",
+            choices: ["Yes", "No"]
+        }).then(function(response){
+            if (response.response === "Yes"){
+                addEmployee();
             }
             else{
                 menu();
